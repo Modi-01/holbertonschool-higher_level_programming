e390c311ddbffd90d5056f69de3b9df7928a500a
Modi-01 <moudhi.a.almutlaq@gmail.com>
2026-01-21 14:49:53 +0000

    Add multiply_by_2 function to return new dictionary with doubled values

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Module: 9-multiply_by_2
-Contains a function that returns a new dictionary with all values multiplied by 2.
+Return a new dictionary where all values are multiplied by 2.
 """
 
 
