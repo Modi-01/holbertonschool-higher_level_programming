7980f03feb65cbef76daf1a4642ff3d02283041d
Modi-01 <moudhi.a.almutlaq@gmail.com>
2026-01-21 16:10:11 +0000

    Fix doctest by importing add_integer in test file

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -9,4 +9,11 @@ def add_integer(a, b=98):
     if type(b) not in (int, float):
         raise TypeError("b must be an integer")
 
+    if type(a) is float:
+        if a != a or a in (float("inf"), float("-inf")):
+            raise TypeError("a must be an integer")
+    if type(b) is float:
+        if b != b or b in (float("inf"), float("-inf")):
+            raise TypeError("b must be an integer")
+
     return int(a) + int(b)
