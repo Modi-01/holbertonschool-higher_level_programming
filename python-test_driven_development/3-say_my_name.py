fe7e957cf449804fd1e74aa6e615bb74f8bb901d
Modi-01 <moudhi.a.almutlaq@gmail.com>
2026-01-23 17:19:06 +0000

    Fix say_my_name output formatting and strengthen doctests

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,14 +3,15 @@
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints: My name is <first_name> <last_name>.
+    """Prints 'My name is <first name> <last name>'.
 
     Args:
         first_name (str): First name.
-        last_name (str): Last name.
+        last_name (str): Last name (optional, defaults to "").
 
     Raises:
-        TypeError: If first_name or last_name is not a string.
+        TypeError: If first_name is not a string.
+        TypeError: If last_name is not a string.
     """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
