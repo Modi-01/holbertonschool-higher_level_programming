577e90585d426c969044541308602a4a8e3933e7
Modi-01 <moudhi.a.almutlaq@gmail.com>
2026-01-23 17:31:25 +0000

    Add print_square function and complete doctest coverage

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-This module provides a function that prints a square using the character #.
-"""
+"""Module that defines a function to print a square with the character #."""
 
 
 def print_square(size):
@@ -14,10 +12,15 @@ def print_square(size):
         TypeError: If size is not an integer.
         ValueError: If size is less than 0.
     """
-    if isinstance(size, float) and size < 0:
+    if type(size) is bool:
         raise TypeError("size must be an integer")
+
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+
     if type(size) is not int:
         raise TypeError("size must be an integer")
+
     if size < 0:
         raise ValueError("size must be >= 0")
 
