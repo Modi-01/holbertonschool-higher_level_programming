2772638ec0e32076842d59a5b071048e7e3a9a03
Modi-01 <moudhi.a.almutlaq@gmail.com>
2026-02-02 14:57:03 +0000

    Add class student

---
+++ python-input_output/9-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module that defines a Student class.
+"""
+
+
+class Student:
+    """
+    Represents a student with a first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a Student instance.
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieve the dictionary representation of a Student instance.
+
+        Returns:
+            dict: The dictionary representation of the student.
+        """
+        return self.__dict__
