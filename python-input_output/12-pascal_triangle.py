51b4dd252fd45e112e73ce3e413876009198943b
Modi-01 <moudhi.a.almutlaq@gmail.com>
2026-02-02 15:05:09 +0000

    Add function that returns a list of lists of integers representing the Pascal's triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Function that generates Pascal's triangle up to a given number of rows.
+"""
+
+
+def pascal_triangle(n):
+    """
+    Generate Pascal's triangle as a list of lists of integers.
+
+    Args:
+        n (int): The number of rows in Pascal's triangle.
+
+    Returns:
+        list: A list of lists representing Pascal's triangle.
+    """
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
