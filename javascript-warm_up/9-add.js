ca95b184cede1ec0cd07d15174b00f21a7f727fa
Modi-01 <moudhi.a.almutlaq@gmail.com>
2026-03-11 09:47:52 +0000

    Add task9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
+
 function add (a, b) {
   return a + b;
 }
 
-module.exports = { add };
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+console.log(add(a, b));
